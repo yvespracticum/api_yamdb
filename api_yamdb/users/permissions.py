15fd97bcb970fd203e3cd1d4bf35808d7e4fbf99
Valery Rabchenyuk <valery0623@gmail.com>
2024-12-05 16:36:35 +0300

    permissions

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,10 +1,4 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-
-
-class IsAuthorOrReadOnly(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        return request.method in SAFE_METHODS or obj.author == request.user
+from rest_framework.permissions import BasePermission
 
 
 class IsAdmin(BasePermission):
