5394c684ab5a353c5463bff68e8fabc3b8530727
Valery Rabchenyuk <valery0623@gmail.com>
2024-12-05 16:57:22 +0300

    minor fixes

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -5,26 +5,16 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserViewSet, get_token, signup
 
 router = DefaultRouter()
-router.register("users", UserViewSet)
+router.register('users', UserViewSet)
 
 auth_patterns = [
     path('signup/', signup, name='signup'),
-    path('token/', get_token, name='get_jwt_token'),
+    path('token/', get_token, name='get_token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
 
 urlpatterns = [
     path('v1/auth/', include(auth_patterns)),
-    path('v1/', include(router.urls)),]  # проверить можно ли оставить '', а в1 перенести в апи_ябд
-
-
-
-
-# urlpatterns = [
-#     ...
-#     # Djoser создаст набор необходимых эндпоинтов.
-#     # базовые, для управления пользователями в Django:
-#     path('auth/', include('djoser.urls')),
-#     # JWT-эндпоинты, для управления JWT-токенами:
-#     path('auth/', include('djoser.urls.jwt')),
-# ]
+    path('v1/', include(router.urls)),
+    ]
