93428ee3f60e144c284faf5df561d6175388302c
OlegMorty <149554882+OlegMorty@users.noreply.github.com>
2024-12-07 12:20:52 +0300

    3 rd developer (#8)
    * добавил комменты, посмотрите на наличие ошибок
    
    * Добавлены модели Отзывов и комментариев 06.12.24
    
    * добавил комменты, посмотрите на наличие ошибок
    
    * Добавлены модели Отзывов и комментариев 06.12.24
    
    * Добавлен ревьюсериализатор и чуть исправлен комментсериализатор
    
    * Поковырял отзывы и комменты 12/68 прогресса. Поменял пермишены пока. Чуть полазил в админке

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,14 @@
 from rest_framework.routers import DefaultRouter
 
 from api.views import (CategoryViewSet, CommentViewSet,
-                       GenreViewSet, TitleViewSet)
+                       GenreViewSet, ReviewViewSet, TitleViewSet)
 
 router = DefaultRouter()
 router.register('titles', TitleViewSet, basename='titles')
 router.register('genres', GenreViewSet, basename='genres')
 router.register('categories', CategoryViewSet, basename='categories')
-router.register(r'titles/(?P<title_id>\d+)/comments',
-                CommentViewSet, basename='comments')  # New
+router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review')
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments', 
+                CommentViewSet, basename='comments')
 
 urlpatterns = router.urls
