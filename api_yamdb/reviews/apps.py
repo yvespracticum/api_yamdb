0ffa3489971e3150cd5fb600958355f0fb1a3cb9
yvespracticum <yvveeessss@ya.ru>
2024-12-11 15:49:21 +0300

    Count rating with methods inside Review model, instead of signals

--- api_yamdb/reviews/apps.py
+++ api_yamdb/reviews/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ReviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
-
-    def ready(self):
-        from reviews.signals import setup
-        setup()
