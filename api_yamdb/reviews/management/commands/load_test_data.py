68a2250ab7d314dfe86c25669f20db9c17d97627
yvespracticum <yvveeessss@ya.ru>
2024-12-27 23:21:18 +0300

    Осталось 2 Лерины поправки

--- api_yamdb/reviews/management/commands/load_test_data.py
+++ api_yamdb/reviews/management/commands/load_test_data.py
@@ -8,14 +8,14 @@ class Command(BaseCommand):
             "relations.")
 
     def handle(self, *args, **kwargs):
-        commands = [
-            ["python", "manage.py", "load_categories",
-             "static/data/category.csv"],
-            ["python", "manage.py", "load_genres", "static/data/genre.csv"],
-            ["python", "manage.py", "load_titles", "static/data/titles.csv"],
-            ["python", "manage.py", "load_genre_title",
-             "static/data/genre_title.csv"],
-        ]
+        commands = (
+            ("python", "manage.py", "load_categories",
+             "static/data/category.csv"),
+            ("python", "manage.py", "load_genres", "static/data/genre.csv"),
+            ("python", "manage.py", "load_titles", "static/data/titles.csv"),
+            ("python", "manage.py", "load_genre_title",
+             "static/data/genre_title.csv"),
+        )
 
         for command in commands:
             self.stdout.write(
